de21c00ca0662f60fb4e27b1afd4395e608be540
cwellis <cwellis2016@gmail.com>
2022-07-30 08:03:59 -0400

    adding current frontend work completed

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ProfileSide from '../../components/ProfileSide/ProfileSide'
+import PostSide from '../../components/PostSide/PostSide'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <ProfileSide />
+      <PostSide />
+      <div className="rightSide">Right</div>
+    </div>
+  )
+}
+
+export default Home
